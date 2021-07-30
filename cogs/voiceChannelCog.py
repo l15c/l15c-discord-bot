d7842cf14c0bcde0e0ebbfbe524f4f6c7a5f2567
L15C <65762908+l15c@users.noreply.github.com>
2021-07-30 14:34:46 +0700

    Update Delete VoiceChannel

--- cogs/voiceChannelCog.py
+++ cogs/voiceChannelCog.py
@@ -1,33 +1,31 @@
-
 from discord.ext import commands
-
-
-
 
 class Activities(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
-    async  def on_voice_state_update(self, member, before, after):
+    async def on_voice_state_update(self, member, before, after):
         if member.bot:
             return
+
         if after.channel is not None and before.channel is not None:
             if before.channel.id == after.channel.id:
                 return
+
         if after.channel is not None:
-            if after.channel.name =="🔑・tạo phòng":
+            if after.channel.name == "🔑・tạo phòng":
                 if before.channel is not None:
-                    if before.channel.name == f"{member.name}'s Room":
+                    if before.channel.name != "🔑・tạo phòng" and len(before.channel.members) == 0:
                         await before.channel.delete()
                 channel = await after.channel.category.create_voice_channel(f"{member.name}'s Room")
                 return await  member.move_to(channel)
         else:
-            if before.channel.name == f"{member.name}'s Room":
+            if len(before.channel.members) == 0:
                 return await before.channel.delete()
 
         if after.channel is not None and before.channel is not None:
-            if before.channel.name == f"{member.name}'s Room" and after.channel !="🔑・tạo phòng":
+            if (before.channel.name != "🔑・tạo phòng" and len(before.channel.members) == 0) and after.channel.name != "🔑・tạo phòng":
                 await before.channel.delete()
 
 
